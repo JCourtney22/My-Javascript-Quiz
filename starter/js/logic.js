8eca7370fcbf487171a672112dccd7b626423003
JCourtney22 <115502589+JCourtney22@users.noreply.github.com>
2022-12-04 14:08:24 +0000

    Start screen disappears now when start button is pressed.

--- starter/js/logic.js
+++ starter/js/logic.js
@@ -14,13 +14,15 @@ var questions = [
 
 var timerEl = document.querySelector("#time");
 var startBtn = document.querySelector("#start");
+// var startScreen = document.querySelector("#start-screen");
 
 var timer;
 var timerCount;
 
 //Start the game function
 function startGame(){
-    timerCount = 10;
+    timerCount = 60;
+    document.getElementsByClassName("start-screen").style.display = "none";
     startTimer()
 }
 
@@ -37,3 +39,19 @@ function startTimer (){
 
 //Event listeners
 startBtn.addEventListener("click", startGame);
+
+//Function for questions
+
+function selectQuestions(){
+    
+}
+
+// question.textContent = currentQuestion.question;
+//  A.textContent = currentQuestion.A;
+//  B.textContent = currentQuestion.B;
+//  C.textContent = currentQuestion.C;
+//  D.textContent = currentQuestion.D;
+
+
+// startScreen.addEventListener("click", startTimer);
+
